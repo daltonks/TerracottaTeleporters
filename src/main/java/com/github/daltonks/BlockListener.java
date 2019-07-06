99fa197f2f886d5da96845697e3b4614d40fcfe8
Dalton Spillman <dalton_spillman@yahoo.com>
2019-07-06 10:36:27 -0500

    Created TerracottaBlocks

--- src/main/java/com/github/daltonks/BlockListener.java
+++ src/main/java/com/github/daltonks/BlockListener.java
@@ -1,19 +1,28 @@
 package com.github.daltonks;
 
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
 public class BlockListener implements Listener {
-
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
+        Material material = event.getBlockPlaced().getType();
+        Integer id = TerracottaBlocks.getId(material);
+        if(id != null) {
 
+        }
     }
 
     @EventHandler
     public void onBlockBreak(BlockBreakEvent event) {
+        Material material = event.getBlock().getType();
+        Integer id = TerracottaBlocks.getId(material);
+        if(id != null) {
 
+        }
     }
 }
