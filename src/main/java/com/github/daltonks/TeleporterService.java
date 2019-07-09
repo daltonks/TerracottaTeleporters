1bd26d0eed64352b3ccc9c4db823d48ab91e0ce3
Dalton Spillman <dalton_spillman@yahoo.com>
2019-07-09 00:38:37 -0500

    TeleporterRepo.getNext work

--- src/main/java/com/github/daltonks/TeleporterService.java
+++ src/main/java/com/github/daltonks/TeleporterService.java
@@ -9,47 +9,10 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 
 public class TeleporterService implements Listener {
-    private static final HashMap<Material, Integer> TERRACOTTA_MATERIAL_TO_ID_MAP = new HashMap<>();
-
-    static {
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.BLACK_TERRACOTTA, 0);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.BLUE_TERRACOTTA, 1);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.BROWN_TERRACOTTA, 2);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.CYAN_TERRACOTTA, 3);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.GRAY_TERRACOTTA, 4);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.GREEN_TERRACOTTA, 5);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.LIGHT_BLUE_TERRACOTTA, 6);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.LIGHT_GRAY_TERRACOTTA, 7);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.LIME_TERRACOTTA, 8);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.MAGENTA_TERRACOTTA, 9);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.ORANGE_TERRACOTTA, 10);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.PINK_TERRACOTTA, 11);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.PURPLE_TERRACOTTA, 12);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.RED_TERRACOTTA, 13);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.WHITE_TERRACOTTA, 14);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.YELLOW_TERRACOTTA, 15);
-
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.BLACK_GLAZED_TERRACOTTA, 16);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.BLUE_GLAZED_TERRACOTTA, 17);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.BROWN_GLAZED_TERRACOTTA, 18);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.CYAN_GLAZED_TERRACOTTA, 19);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.GRAY_GLAZED_TERRACOTTA, 20);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.GREEN_GLAZED_TERRACOTTA, 21);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.LIGHT_BLUE_GLAZED_TERRACOTTA, 22);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.LIGHT_GRAY_GLAZED_TERRACOTTA, 23);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.LIME_GLAZED_TERRACOTTA, 24);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.MAGENTA_GLAZED_TERRACOTTA, 25);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.ORANGE_GLAZED_TERRACOTTA, 26);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.PINK_GLAZED_TERRACOTTA, 27);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.PURPLE_GLAZED_TERRACOTTA, 28);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.RED_GLAZED_TERRACOTTA, 29);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.WHITE_GLAZED_TERRACOTTA, 30);
-        TERRACOTTA_MATERIAL_TO_ID_MAP.put(Material.YELLOW_GLAZED_TERRACOTTA, 31);
-    }
-
     private final TeleporterRepo repo;
 
     public TeleporterService(TeleporterRepo repo) {
@@ -57,13 +20,35 @@ public class TeleporterService implements Listener {
     }
 
     @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        Block block = event.getBlockPlaced();
+    public void onBlockPlace(BlockPlaceEvent event) throws SQLException {
+        addOrUpdateTeleporterAt(event.getBlockPlaced());
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) throws SQLException {
+        ItemStack itemStack = event.getItem();
+        Block clickedBlock = event.getClickedBlock();
+        if(itemStack != null && itemStack.getType() == Material.CLAY_BALL && clickedBlock != null) {
+            Teleporter teleporter = addOrUpdateTeleporterAt(clickedBlock);
+            if(teleporter != null) {
+                Teleporter nextTeleporter = repo.getNext(teleporter);
+                if(nextTeleporter != null) {
+                    // TODO: Keep trying the next teleporter until one
+                    // is found that isn't deleted in the world
+
+                    // TODO: Teleport player
+                    itemStack.setAmount(itemStack.getAmount() - 1);
+                }
+            }
+        }
+    }
+
+    private Teleporter addOrUpdateTeleporterAt(Block block) throws SQLException {
         Material material = block.getType();
 
         if(material == Material.GOLD_BLOCK) {
             Block topBlock = block.getWorld().getBlockAt(block.getX(), block.getY() + 1, block.getZ());
-            Integer terracottaMaterialId = TERRACOTTA_MATERIAL_TO_ID_MAP.get(topBlock.getType());
+            Integer terracottaMaterialId = TerracottaMaterialIDs.getID(topBlock.getType());
             if(terracottaMaterialId != null) {
                 Teleporter teleporter = new Teleporter(
                     block.getWorld().getName(),
@@ -73,10 +58,10 @@ public class TeleporterService implements Listener {
                     terracottaMaterialId
                 );
 
-                repo.add(teleporter);
+                repo.addOrUpdate(teleporter);
             }
         } else {
-            Integer terracottaMaterialId = TERRACOTTA_MATERIAL_TO_ID_MAP.get(block.getType());
+            Integer terracottaMaterialId = TerracottaMaterialIDs.getID(block.getType());
             if(terracottaMaterialId != null) {
                 Block bottomBlock = block.getWorld().getBlockAt(block.getX(), block.getY() - 1, block.getZ());
                 if(bottomBlock.getType() == Material.GOLD_BLOCK) {
@@ -88,17 +73,11 @@ public class TeleporterService implements Listener {
                         terracottaMaterialId
                     );
 
-                    repo.add(teleporter);
+                    repo.addOrUpdate(teleporter);
                 }
             }
         }
-    }
 
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event) {
-        ItemStack itemStack = event.getItem();
-        if(itemStack != null && itemStack.getType() == Material.CLAY_BALL) {
-            
-        }
+        return null;
     }
 }
--- src/main/java/com/github/daltonks/sqlite/TeleporterRepo.java
+++ src/main/java/com/github/daltonks/sqlite/TeleporterRepo.java
@@ -4,6 +4,7 @@ import com.github.daltonks.Teleporter;
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,8 +18,8 @@ public class TeleporterRepo {
         this.logger = logger;
     }
 
-    public void add(Teleporter teleporter) {
-        String sql = "INSERT INTO Teleporter VALUES (?, ?, ?, ?, ?)";
+    public void addOrUpdate(Teleporter teleporter) throws SQLException {
+        String sql = "INSERT OR REPLACE INTO Teleporter VALUES (?, ?, ?, ?, ?)";
         try (PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
             statement.setString(1, teleporter.getWorldName());
             statement.setInt(2, teleporter.getX());
@@ -27,12 +28,10 @@ public class TeleporterRepo {
             statement.setInt(5, teleporter.getTerracottaMaterialId());
 
             statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Adding teleporter error", e);
         }
     }
 
-    public void delete(Teleporter teleporter) {
+    public void delete(Teleporter teleporter) throws SQLException {
         String sql = "DELETE FROM Teleporter WHERE WorldName = ? AND X = ? AND Y = ? AND Z = ?";
         try (PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
             statement.setString(1, teleporter.getWorldName());
@@ -41,12 +40,73 @@ public class TeleporterRepo {
             statement.setInt(4, teleporter.getZ());
 
             statement.executeUpdate();
-        } catch (SQLException e) {
-            logger.log(Level.SEVERE, "Deleting teleporter error", e);
         }
     }
 
-    public Teleporter getNext(Teleporter teleporter) {
-        throw new NotImplementedException();
+    public Teleporter getNext(Teleporter teleporter) throws SQLException {
+        int rowId;
+
+        // Get rowid
+        String sql = "SELECT rowid FROM Teleporter WHERE WorldName = ? AND X = ? AND Y = ? AND Z = ?";
+        try (PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
+            statement.setString(1, teleporter.getWorldName());
+            statement.setInt(2, teleporter.getX());
+            statement.setInt(3, teleporter.getY());
+            statement.setInt(4, teleporter.getZ());
+
+            try(ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+                rowId = resultSet.getInt("rowid");
+            }
+        }
+
+        // Try to get next teleporter with a greater row ID
+        sql = "SELECT MIN(rowid) FROM Teleporter WHERE Material = ? AND rowid > ?";
+        try(PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
+            statement.setInt(1, teleporter.getTerracottaMaterialId());
+            statement.setInt(2, rowId);
+
+            try(ResultSet resultSet = statement.executeQuery()) {
+                if(resultSet.next()) {
+                    int nextRowId = resultSet.getInt("rowid");
+                    return get(nextRowId);
+                }
+            }
+        }
+
+        // Try to get next teleporter with a lesser row ID
+        sql = "SELECT MIN(rowid) FROM Teleporter WHERE Material = ? AND rowid < ?";
+        try(PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
+            statement.setInt(1, teleporter.getTerracottaMaterialId());
+            statement.setInt(2, rowId);
+
+            try(ResultSet resultSet = statement.executeQuery()) {
+                if(resultSet.next()) {
+                    int nextRowId = resultSet.getInt("rowid");
+                    return get(nextRowId);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private Teleporter get(int rowId) throws SQLException {
+        String sql = "SELECT * FROM Teleporter WHERE rowid = ?";
+        try (PreparedStatement statement = db.getConnection().prepareStatement(sql)) {
+            statement.setInt(1, rowId);
+
+            try(ResultSet resultSet = statement.executeQuery()) {
+                resultSet.next();
+
+                return new Teleporter(
+                    resultSet.getString("WorldName"),
+                    resultSet.getInt("X"),
+                    resultSet.getInt("Y"),
+                    resultSet.getInt("Z"),
+                    resultSet.getInt("Material")
+                );
+            }
+        }
     }
 }
